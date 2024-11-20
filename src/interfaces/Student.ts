cb48518ee11214b08749c617e22a6965b766edff
tony0831-L <64834890+tony0831-L@users.noreply.github.com>
2024-11-21 02:48:59 +0800

    init

---
+++ src/interfaces/Student.ts
@@ -0,0 +1,10 @@
+export interface Student{
+    userName: string,
+    sid: number,
+    name: string,
+    department: string,
+    grade: string,
+    class:string,
+    Email: string,
+    absences?: number|undefined
+}
